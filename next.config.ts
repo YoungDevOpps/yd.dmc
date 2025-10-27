be7629d8ad1e8f7ce647adce0875f03905b7d8e4
unknown <tangaraayves@gmail.com>
2025-10-27 20:09:34 +0000

    build app

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Permet de générer un site statique (si besoin)
+  output: "export",
+
+  // Dossier de sortie du build
+  distDir: "build",
+
   /* config options here */
   images: {
+    unoptimized: true, // désactive l’optimisation automatique (utile pour export statique)
     remotePatterns: [
       {
         protocol: "https",
